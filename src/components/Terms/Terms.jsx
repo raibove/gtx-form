13c888f253b5efc4eaf4c03ff4eaf83600d63148
raibove <shwetakale144@gmail.com>
2023-03-25 23:41:31 +0530

    update style and add google font

--- src/components/Terms/Terms.jsx
+++ src/components/Terms/Terms.jsx
@@ -1,20 +1,24 @@
 import ButtonContainer from "../ButtonContainer/ButtonContainer";
+import "./Terms.css";
 
-const Terms = ()=>{
-    return(
-        <div>
-            <h3>Up-skilling requires time commitment</h3>
-            <p>The GrowthX experience is designed by keeping in mind the working hours founders & full time operators typically work in.</p>
-            <p>
-            You will spend
-            <ul>
-                <li> 6 hours/week for the first 5 weeks</li>
-                <li> 15 hours/week for the last 3 weeks</li>
-            </ul>
-            </p>
-            <ButtonContainer buttonText="I agree" showPressEnter={true} />
-        </div>
-    )
-}
+const Terms = () => {
+  return (
+    <div className="terms-container">
+      <span className="terms-title">Up-skilling requires time commitment</span>
+      <p className="terms-description">
+        The GrowthX experience is designed by keeping in mind the working hours
+        founders & full time operators typically work in.
+        <br />
+        <br />
+        <span>You will spend</span>
+        <br />
+        <span>- 6 hours/week for the first 5 weeks</span>
+        <br />
+        <span>- 15 hours/week for the last 3 weeks</span>
+      </p>
+      <ButtonContainer buttonText="I agree" showPressEnter={true} />
+    </div>
+  );
+};
 
 export default Terms;
